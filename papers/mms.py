d75556283fb6285e676ff94a1ae2f72cf75735f3
dileepNaidu <dileepnaiduone@gmail.com>
2024-11-29 20:41:47 +0530

    Added Loading Animation in LLM SummaryPage

--- papers/mms.py
+++ papers/mms.py
@@ -12,20 +12,25 @@ with open( "style.css" ) as css:
 try:
 
     st.image(r"./img/logo-light.svg", width=100)
-    if "text_shown" not in st.session_state:
-        st.write(make_me_smart(st.session_state["input_topic"], st.session_state["check_list"]))
-        st.session_state["text_shown"] = True
-
-
-    st.write("\n")
     st.write("\n")
     st.write("\n")
     st.write("\n")
 
-    return_button = st.button("RETURN TO HOME", type="primary")
+    with st.spinner(f"Hang tight! :gray[Your quiz summary is on the way.] :red[Meanwhile, can you guess which question caught your attention the most?]"):
+        if "text_shown" not in st.session_state:
+            st.write(make_me_smart(st.session_state["input_topic"], st.session_state["check_list"]))
+            st.session_state["text_shown"] = True
 
-    if return_button:
-        st.switch_page("papers/home.py")
+
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+        st.write("\n")
+
+        return_button = st.button("RETURN TO HOME", type="primary")
+
+        if return_button:
+            st.switch_page("papers/home.py")
 
 except KeyError:
     st.switch_page("papers/reload.py")
